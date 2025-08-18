5bebdfe7bcbfbbf3ca12384d53b9a87c55254776
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-08-18 20:44:41 -0300

    feat: implement assets

---
+++ assets/assets.d.ts
@@ -0,0 +1,29 @@
+declare module "*.png" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+}
+
+declare module "*.jpeg" {
+    const value: any;
+    export default value;
+}
+
+export interface Assets {
+    bannerImage: any;
+    banner2: any;
+    bellaItalia: any;
+    tokioSushi: any;
+    roseGarden: any;
+    pizzaCategory: any;
+    hamburgerCategory: any;
+    sushiCategory: any;
+    logotipo: any;
+    background: any;
+    afro: any;
+    monicaSantos: any;
+}
---
+++ assets/assets.ts
@@ -0,0 +1,16 @@
+import { Assets } from "./assets.d";
+
+export const assets: Assets = {
+    bannerImage: require("./banner1.png"),
+    banner2: require("./banner2.png"),
+    bellaItalia: require("./bella-italia.jpeg"),
+    tokioSushi: require("./tokio-sushi.png"),
+    roseGarden: require("./rose-garden.png"),
+    pizzaCategory: require("./pizza-category.jpeg"),
+    hamburgerCategory: require("./hamburger-category.jpeg"),
+    sushiCategory: require("./sushi-category.jpeg"),
+    logotipo: require("./logotipo.png"),
+    background: require("./background.jpeg"),
+    afro: require("./afro.jpg"),
+    monicaSantos: require("./monica_santos.jpg"),
+};
